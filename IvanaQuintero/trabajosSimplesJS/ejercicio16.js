d707dcdb5b27299b10bc05a4689df81dc9235517
ivana Quintero <ivanaquintero18@gmail.com>
2022-09-28 16:05:24 -0700

    agregando directorio

---
+++ IvanaQuintero/trabajosSimplesJS/ejercicio16.js
@@ -0,0 +1,6 @@
+var firstNameLength = 0;
+var firstName = "ivana";
+firstNameLength = firstName.length;
+var LastNameLength = 0;
+var lastName = "quintero";LastNameLength = LastName.length;
+console.log(LastNameLength)
