976ecd717f5109aaa943a0bd7f2eb53d7a5c1500
Alfredo Munoz <fofitomn@gmail.com>
2022-09-28 08:40:40 -0700

    agregandocodigo

---
+++ alfredomuñoz/longname.js
@@ -0,0 +1,10 @@
+var firstNameLength = 0;
+var firstName = "Alfredo";
+
+firstNameLength = firstName.length;
+
+var lastNameLength = 0;
+var lastName = "Muñoz";
+
+lastNameLength = lastName.length;
+console.log("There are "+ lastNameLength + " letter/s in your last name")
