4467a528ecac74c418c51646ecf89a000cc0e5cd
Celic Hernandez <10ximenahdzama@gmail.com>
2022-09-30 07:59:04 -0500

    Agregando segunda parte

---
+++ celiccc/primerproyecto/src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import './App.css';
+import contador from './components/contador'
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Titulo primer proyecto </h1>
+   < contador></contador>
+     <hola></hola>
+    </div>
+  );
+}
+
+export default App;
