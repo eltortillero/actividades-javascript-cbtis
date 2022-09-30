6a2fcd2d5dff0c4ef4250c4740d0924c87b0dc98
ivana Quintero <ivanaquintero18@gmail.com>
2022-09-29 18:57:15 -0700

    agregando primera sesion

---
+++ IvanaQuintero/UdemyCurso1/ejercicio8.js
@@ -0,0 +1,11 @@
+const datos = [
+{nombre:'ivana', edad:17},
+{nombre:'violeta', edad:17},
+{nombre:'jazmin', edad:17},
+{nombre:'annethe', edad:17},
+
+]
+
+datos.map(elemento => {
+    console.log(elemento.nombre)
+})
