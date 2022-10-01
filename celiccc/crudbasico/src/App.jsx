648b568bb8b3fc1f32f39dd62ed43125115d9309
Celic Hernandez <10ximenahdzama@gmail.com>
2022-10-01 09:41:00 -0500

    Agregando parte tres

---
+++ celiccc/crudbasico/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Listadonombre from '../componente2/Listadonombres'
+
+function App() {
+  return (
+    <div className="container">
+     <Listadonombre/>
+
+     
+    </div>
+  );
+}
+
+export default App;
