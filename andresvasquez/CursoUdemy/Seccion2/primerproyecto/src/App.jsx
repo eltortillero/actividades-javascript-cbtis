a0498f1a434fca73c033fbae9dbf21119feda6f8
Nicolas Vasquez <Lord_of_the_frogs@outlook.com>
2022-10-03 16:14:29 -0700

    Cambiando codigo de direccion

--- andresvasquez/CursoUdemy/Seccion2/primerproyecto/src/App.jsx
+++ andresvasquez/CursoUdemy/Seccion2/primerproyecto/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import Formulario from './components/Formulario';
+//import Formulario from './components/Formulario';
 
 
 
 //import Temperatura from './components/Temperatura';
 //import Listado from './components/Listado';
-//import  Contador  from './components/contador';
+import  Contador  from './components/contador';
 function App() {
   return (
     <div className="App">
       <h1>Titulo de nuestro primer proyecto</h1>
-      <Formulario></Formulario>
+      <Contador></Contador>
       </div>
 
   );
