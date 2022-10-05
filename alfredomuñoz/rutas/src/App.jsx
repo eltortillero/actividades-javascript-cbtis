1683456b8126d5c0bfd29a1418bd8a2158fc744d
Alfredo Munoz <fofitomn@gmail.com>
2022-10-05 11:13:42 -0700

    agregando cambios

--- alfredomuñoz/rutas/src/App.jsx
+++ alfredomuñoz/rutas/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Inicio from './components/inicio';
-import Base from './components/Base';
+
 import {
   BrowserRouter as Router,
+  Routes,
   Route,
   Link
 } from 'react-router-dom'
@@ -13,18 +13,16 @@ function App() {
 <Link to ="/inicio">Inicio</Link>
 
 <Link to ="/">Base</Link>
-
+  <Routes>
   <Route exact path="/">
-  <base/>
 </Route>
 
  <Route path="/:nombre">
 
- <Inicio/>
 
  </Route>
-
-
+</Routes>
+ 
 
    </Router>
   );
