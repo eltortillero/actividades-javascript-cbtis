e86b31c92ebae61904c04e8c8caa3c5b756a71ce
Horeb Gastelum <horeb@live.com.mx>
2022-10-06 16:28:42 -0700

    modular firebase 9

--- celiccc/firebase-auth-udem/src/firebaseconf.js
+++ celiccc/firebase-auth-udem/src/firebaseconf.js
@@ -1,19 +1,18 @@
-import firebase from 'firebase'
-import 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWeVepbboBdIECAGq_7ufiVr8lEVp7FGM",
-    authDomain: "react-proyect-fa224.firebaseapp.com",
-    projectId: "react-proyect-fa224",
-    storageBucket: "react-proyect-fa224.appspot.com",
-    messagingSenderId: "409740681407",
-    appId: "1:409740681407:web:bf13e805abef56bd3ae60a",
-    measurementId: "G-6B3E71R7YE"
-  };
-  
-  // Initialize Firebase
+  apiKey: "AIzaSyBWeVepbboBdIECAGq_7ufiVr8lEVp7FGM",
+  authDomain: "react-proyect-fa224.firebaseapp.com",
+  projectId: "react-proyect-fa224",
+  storageBucket: "react-proyect-fa224.appspot.com",
+  messagingSenderId: "409740681407",
+  appId: "1:409740681407:web:bf13e805abef56bd3ae60a",
+  measurementId: "G-6B3E71R7YE"
+};
 
-  const fire = firebase.initializeApp(firebaseConfig);
-  export const auth = fire.auth()
+// Initialize Firebase
 
-  
+const firebaseAppInstance = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseAppInstance);
+
