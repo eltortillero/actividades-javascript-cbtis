2465b78a677c179aa5a8b3f1e877a70de1528d9c
Nicolas Vasquez <Lord_of_the_frogs@outlook.com>
2022-10-06 23:35:27 -0700

    Agregando Seccion 6 (Completada)

--- andresvasquez/CursoUdemy/Seccion6/firebase-auth-udemy/src/App.jsx
+++ andresvasquez/CursoUdemy/Seccion6/firebase-auth-udemy/src/App.jsx
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Inicio from './components/Inicio';
 import Admin from './components/Admin';
 import Login from './components/Login';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="container">
+      <h1>Hola</h1>
 <Router>
-
+    <Menu></Menu>
 <Routes>
 
   <Route exact to="/" component={Inicio}></Route>
--- andresvasquez/CursoUdemy/Seccion6/firebase-auth-udemy/src/components/Login.jsx
+++ andresvasquez/CursoUdemy/Seccion6/firebase-auth-udemy/src/components/Login.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
+import {auth} from '../firebaseconfig'
+import {useHistory} from 'react-router-dom'
 
 const Login = () => {
+const historial = useHistory()
+const[email, setEmail] = useState('')
+const[pass, setPass] = useState('')
+const [msgerror, setMsgError] = useState(null)
+
+const RegistrarUsuario = (e) =>{
+  e.preventDefault()
+  try{
+    auth.createUserWithEmailAndPassword(email,pass)
+    .then(r => {
+      historial.push('/')
+    })
+    
+  }catch (e){
+    /*"auth/weak-password"*/
+    if(e.code == 'auth/invalid-email'){
+      setMsgError('Formado Email incorrecto')
+    }
+    if(e.code == 'auth/weak-password'){
+      setMsgError('La contraseña debe tener 6 caracteres o mas')
+    }
+  }
+}
+
+const LoginUsuario = () =>{
+  auth.signInWithEmailAndPassword(email,pass)
+  .then( (r) => {
+    historial.push('/')
+  })
+  .catch( (err) =>{
+    if(err.code == 'auth/wrong-password'){
+      setMsgError('La contraseña es incorrecta')
+    }
+  })
+}
+
   return (
-    <div>
-        <h2>Login</h2>
+    <div className='row mt-5'>
+        <div className='col'></div>
+        <div className='col'>
+          <form onSubmit={RegistrarUsuario} className='form-group'>
+            <input 
+            onChange={(e) => {setEmail(e.target.value) }}
+            className='form-control'
+            placeholder='Introduce el Email'
+            type="email"/>
+            <input 
+            onChange={(e) => {setPass(e.target.value) }}
+            className='form-control mt-4'
+            placeholder='Introduce la Contrasenia'
+            type="password"/>
+            <input 
+            className='btn btn-dark btn-block mt-4'
+            value='Registrar Usuario'
+            type="submit"/>
+            </form>    
+            <button onClick={LoginUsuario}
+            className='btn btn-success btn-block'>
+              Iniciar sesion
+              </button>   
+            {
+              /*msgerror != null ?
+              (
+                <div>
+                {msgerror}
+                </div>
+              )
+              :
+              (
+                <span>
+              )*/
+            }   
+        </div>
+        <div className='col'></div>
     </div>
   )
 }
